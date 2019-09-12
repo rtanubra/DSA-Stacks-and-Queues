863f48c5241ebe0d1ca4e5cb3490bb25f78a6e9d
rtanubra <rdtanubrata@gmail.com>
2019-09-12 08:01:35 -0400

    completed number 5

--- Stack.js
+++ Stack.js
@@ -35,6 +35,11 @@ function peek(Stack){
     console.log(`Top of the stack is ${Stack.top.data}`)
 }
 function display(Stack){
+    if (Stack.top===null){
+        console.log(`***********This side top********`)
+        console.log('\nnull\n')
+        return
+    }
     var currentNode = Stack.top
     console.log(`***********This side top********`)
     while (currentNode.next !== null){
@@ -43,6 +48,131 @@ function display(Stack){
     }
     console.log(currentNode.data)
 }
+
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    const myStack = new Stack()
+    var count = 0 
+    while (count< s.length){
+        myStack.push(s[count])
+        count +=1
+    }
+
+    var top 
+    count = 0 
+    while (count < s.length){
+        if (s[count] != myStack.pop()){
+            return false
+        }
+        count +=1
+    }
+    return true
+}
+
+function matchingParantheses(myStr){
+    const myStack = new Stack()
+    var count = 0 
+    while (count< myStr.length){
+        myStack.push(myStr[count])
+        count +=1
+    }
+    var open = false 
+    var currentItem = false 
+    var current = myStack.top
+
+    while (current.next !== null){
+        if (current.data === ")"){
+            if (open===true){
+                //openned when we are bracket is already open
+    
+                return false
+            }
+            open = true
+        }
+        if (current.data === "("){
+            if (open===false){
+  
+                return false
+            }
+            open = false
+        }
+        current = current.next
+    }
+    if(open===true && current.data !=="(" ){
+
+        return false
+    }
+    return true
+}
+
+function sortStack(array){
+    var stack1 = new Stack()
+    var stack2 = new Stack()
+    const stackFinal = new Stack()
+    
+    var count = 0 
+    while (count< array.length){
+        stack1.push(array[count])
+        count +=1
+    }
+    var checking = true
+    var smallest =1000000000
+    var current
+    var populatedStack = stack1
+    var emptyStack = stack2
+    while (stack1.top!==null || stack2.top !==null){
+        //iterate through the populated stack to find the smallest
+        if (checking===true){
+            //name temporary stacks
+            populatedStack = stack1
+            emptyStack = stack2
+
+            //unstack populated to stack emptyStack
+            while (populatedStack.top !==null){
+                current = populatedStack.pop()
+                if (current<smallest){
+                    smallest = current
+                }
+                emptyStack.push(current)
+            }
+            checking = false
+            stack1 = emptyStack
+            stack2 = populatedStack
+        }
+        //iterate through the stack and grab the smallest
+        else{
+
+            //name temporary stacks
+            populatedStack = stack1
+            emptyStack = stack2
+
+            //unstack populated to stack emptyStack
+            while (populatedStack.top !==null){
+                current = populatedStack.pop()
+                
+                if (smallest===current){
+                    stackFinal.push(current)
+                    smallest = 1000000000
+                }else{
+                    emptyStack.push(current)
+                }
+            }
+            checking = true 
+            stack1 = emptyStack
+            stack2 = populatedStack
+        }
+    }
+
+    //build a return and reverse to show ascending
+    const arr =[]
+    while (stackFinal.top!==null){
+        current = stackFinal.pop()
+        arr.push(current)
+    }
+    return arr.reverse()
+}
+
+
 function main(){
     const starTrek = new Stack()
     const barTrek = new Stack()
@@ -84,64 +214,10 @@ function main(){
     console.log(`\n Test 2 Matching Paranthes`)
     console.log(`Given '(dad)' matchingParantheses() should return true - ${matchingParantheses('(dad)')}`)
 
+    console.log(`\n Test 3 Sorting`)
+    console.log(`Given '[2,3,1,6,8,9] sortStack() - ${sortStack([2,3,1,6,8,9])}`)
+    console.log(`Given '[3,9,76,8,12,34,56,4] sortStack() - ${sortStack([3,9,76,8,12,34,56,4])}`)
+
 }
 
 main()
-
-
-
-function is_palindrome(s) {
-    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-    const myStack = new Stack()
-    var count = 0 
-    while (count< s.length){
-        myStack.push(s[count])
-        count +=1
-    }
-
-    var top 
-    count = 0 
-    while (count < s.length){
-        if (s[count] != myStack.pop()){
-            return false
-        }
-        count +=1
-    }
-    return true
-}
-
-function matchingParantheses(myStr){
-    const myStack = new Stack()
-    var count = 0 
-    while (count< myStr.length){
-        myStack.push(myStr[count])
-        count +=1
-    }
-    var open = false 
-    var currentItem = false 
-    var current = myStack.top
-    display(myStack)
-    while (current.next !== null){
-        if (current.data === ")"){
-            if (open===true){
-                //openned when we are bracket is already open
-                console.log('1')
-                return false
-            }
-            open = true
-        }
-        if (current.data === "("){
-            if (open===false){
-                console.log('2')
-                return false
-            }
-            open = false
-        }
-    }
-    if(open===true){
-        console.log('here')
-        return false
-    }
-    return true
-}
-
